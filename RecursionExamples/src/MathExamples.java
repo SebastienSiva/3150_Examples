546cb0687fff4a80aec1be1857756af90e58a16d
Sebastien Siva <ssiva@ggc.edu>
2025-01-28 14:44:19 -0500

    Lecture 1_28_25

---
+++ RecursionExamples/src/MathExamples.java
@@ -0,0 +1,49 @@
+public class MathExamples {
+    //.length
+    //.charAt...
+    //.subString...
+
+
+    public static int fibRecur(int n) {
+        if(n <= 1)
+            return n;
+        return fibRecur(n-1) + fibRecur(n-2);
+    }
+
+    public static int fibLoop(int n) {
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        while(n > 2) {
+            a = b;
+            b = c;
+            c = a + b;
+            n--;
+        }
+        return c;
+    }
+
+
+
+    // n:      1 2 3 4  5
+    // fac(n): 1 2 6 24 120
+    public static int facLoop(int n) {
+        int p = 1;
+        for(int i = 2; i <= n; i++) {
+            p = p * i;
+        }
+        return p;
+    }
+
+    public static int facRecur(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        return n * facRecur(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibRecur(7));
+        //System.out.println(facRecur(5));
+    }
+}
