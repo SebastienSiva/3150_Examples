8252c7a89f52b8ccd7656df5fa69e7594e504ec8
Sebastien Siva <ssiva@ggc.edu>
2025-03-11 10:07:17 -0400

    HW Solution

--- Ch.5_6/Sets/src/Main.java
+++ Ch.5_6/Sets/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class Main {
     public static int hash(String s) {
         //covert string to unique integer
@@ -5,8 +7,26 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        LinkedList<String> myList = new LinkedList<>();
+        myList.add("Bob");
+        myList.add("Sally");
+
+        for(String s: myList) {
+            System.out.println(s);
+        }
+
+        //BAD IDEA BECUASE JUMPING TO INDEX I IS EXPENSIVE IN LINKEDLIST
+        /*
+        for(int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+         */
+
+
+        /*
         char c = 'A';
         int i = (int) c;
         System.out.println(c + " is " + i);
+        */
     }
 }
--- Ch.5_6/Sets/src/StringHashSet.java
+++ Ch.5_6/Sets/src/StringHashSet.java
@@ -1,20 +1,46 @@
+import java.util.LinkedList;
+
 public class StringHashSet implements StringSet{
     //boolean for every hash value
-    private boolean[] array;
+    private LinkedList<String>[] buckets;
     private int size = 0; //num strings in set
-    private int capacity = 50000; //initial capacity
+    private int capacity = 10; //initial capacity
     private double threshHold = 0.75;
 
     public StringHashSet() {
-        this.array = new boolean[capacity];
+        this.buckets = new LinkedList[capacity];
+        for(int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<>();
+        }
     }
 
     public boolean containsString(String s) {
         int h = Math.abs(s.hashCode());
-        return array[h % array.length];
+        LinkedList<String> bucket = buckets[h % buckets.length];
+        return bucket.contains(s);
     }
 
-    private void resize() {}
+    private void resize() {
+        //TODO: DOUBLE THE CAPACITY OF THE BUCKETS ARRAY
+        capacity *= 2;
+        //NOTE: BOTH THE BUCKETS AND CAPACITY FIELDS SHOULD BE UPDATED
+        //HINT1: CREATE AND INIT A TEMP ARRAY OF LINKEDLISTS
+        LinkedList<String>[] temp = new LinkedList[capacity];
+        for(int i = 0; i < temp.length; i++) {
+            temp[i] = new LinkedList<>();
+        }
+        //HINT2: COPY OVER OLD VALUES FROM EACH OLD BUCKET TO THE CORRECT NEW BUCKET
+        for(int i = 0; i < buckets.length; i++) {
+            LinkedList<String> oldBucket = buckets[i];
+            //HINT3: REMEMBER TO LOOP THROUGH LINKEDLIST USING ENHANCED FOR LOOP
+            for(String s: oldBucket) {
+                int h = Math.abs(s.hashCode());
+                LinkedList<String> newBucket = temp[h % temp.length];
+                newBucket.add(s);
+            }
+        }
+        this.buckets = temp;
+    }
 
     public void addString(String s) {
         if(this.containsString(s)) {
@@ -28,7 +54,8 @@ public class StringHashSet implements StringSet{
 
         // add next string
         int h = Math.abs(s.hashCode());
-        array[h % array.length] = true;
+        LinkedList<String> bucket = buckets[h % buckets.length];
+        bucket.add(s);
         size += 1;
     }
 
@@ -38,8 +65,8 @@ public class StringHashSet implements StringSet{
 
     public String toString() {
         String all = "Set{";
-        for(int i = 0; i < array.length; i++) {
-            all += array[i] + ", ";
+        for(int i = 0; i < buckets.length; i++) {
+            all += buckets[i] + ", ";
         }
         if(size > 0)
             all = all.substring(0, all.length()-2);
@@ -50,8 +77,8 @@ public class StringHashSet implements StringSet{
         StringSet mySet = new StringHashSet();
         for(int i = 0; i < 40; i++) {
             String n = TestUtil.getRandomName();
-            int hashIndex = Math.abs(n.hashCode()) % 50000;
-            System.out.println("Adding: " + n + " at hash index " + hashIndex);
+            //int hashIndex = Math.abs(n.hashCode()) % 10;
+            //System.out.println("Adding: " + n + " at hash index " + hashIndex);
             mySet.addString(n);
         }
         System.out.println(mySet);
