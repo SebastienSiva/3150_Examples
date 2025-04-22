82009fbc83e7a2ddd48d09c7b0d6b1d341e7e7e2
Sebastien Siva <ssiva@ggc.edu>
2025-04-22 12:02:01 -0400

    Map

--- Ch.10/ThreadsStockTrader/src/Main.java
+++ Ch.10/ThreadsStockTrader/src/Main.java
@@ -1,21 +1,23 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         GUI gui = new GUI();
 
-        //PurchaseRequest pr = new PurchaseRequest("NVDA", 75);
-        //Thread t = new Thread(pr);
-        //t.start();
+        PurchaseRequest pr = new PurchaseRequest(gui);
+        Thread t = new Thread(pr);
+        t.start();
 
         int numConnections = 3;
         NASDAQConnectionMgr conMgr = new NASDAQConnectionMgr(numConnections);
-        List<StockTracker> trackers = new ArrayList<>();
+        Map<String, StockTracker> trackers = new HashMap<>();
         for(String s: NASDAQ_DB.getStockNames()) {
             StockTracker track = new StockTracker(s, conMgr);
             new Thread(track).start();
-            trackers.add(track);
+            trackers.put(s, track);
         }
 
         while(true) {
