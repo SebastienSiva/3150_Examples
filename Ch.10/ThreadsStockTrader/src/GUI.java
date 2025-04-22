ae6280ec2f4094950440fe12c0d85adb7670ccfa
Sebastien Siva <ssiva@ggc.edu>
2025-04-22 11:26:24 -0400

    JLabel Array

--- Ch.10/ThreadsStockTrader/src/GUI.java
+++ Ch.10/ThreadsStockTrader/src/GUI.java
@@ -3,12 +3,18 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUI {
+    private JLabel[] stockLabels;
 
     public GUI() {
         //TOP PANEL
         JPanel topPanel = new JPanel();
         topPanel.setPreferredSize(new Dimension(640, 120));
-
+        topPanel.setLayout(new GridLayout(5, 1));
+        stockLabels = new JLabel[5];
+        for(int i = 0; i < 5; i++) {
+            stockLabels[i] = new JLabel("Row " + i)
+            topPanel.add(stockLabels[i]);
+        }
 
         //create window
         JFrame frame = new JFrame ();
@@ -17,7 +23,7 @@ public class GUI {
         Border border = BorderFactory.createEmptyBorder(10, 10, 10, 10);
         topPanel.setBorder(border);
         frame.add(topPanel, BorderLayout.NORTH);
-        
+
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle ("STOCK APP");
